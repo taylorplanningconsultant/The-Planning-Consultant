c84a1d406228ae08667ddff375d2247040fcba72
Taylor Burrows <tbone@Taylors-MacBook-Pro.local>
2026-04-08 22:03:25 +0100

    stripe product updated

--- src/lib/stripe/products.ts
+++ src/lib/stripe/products.ts
@@ -1,21 +1,21 @@
 export const STRIPE_PRODUCTS = {
   oneOff: {
-    fullReport: "price_1TGoYgJRdCrAqoHOjNJUliF0",
-    statement: "price_1TJ0daJRdCrAqoHOX0ayE1Pf",
-    bundle: "price_1TGoXGJRdCrAqoHOiEnQeq52",
+    fullReport: "price_1TJyRCJV85dwM5kFI3rrMoK8",
+    statement: "price_1TJyR8JV85dwM5kFBFaFLhro",
+    bundle: "price_1TJyRKJV85dwM5kFV5A0tEkj",
   },
   topUp: {
-    small: "price_1TJ0egJRdCrAqoHOjyMUeDh8",
-    medium: "price_1TJ0fMJRdCrAqoHOAdmXUCWS",
-    large: "price_1TJ0fjJRdCrAqoHOiNeGyfSu",
+    small: "price_1TJyR3JV85dwM5kFuLsE2wc4",
+    medium: "price_1TJyR0JV85dwM5kFSiHhpFMy",
+    large: "price_1TJyR3JV85dwM5kFkA1JbJXJ",
   },
   subscriptions: {
-    starterMonthly: "price_1TGoc1JRdCrAqoHOzg6lRTnD",
-    starterAnnual: "price_1TGodCJRdCrAqoHO3aItddb1",
-    proMonthly: "price_1TGodbJRdCrAqoHOa7GFwZZR",
-    proAnnual: "price_1TGoe4JRdCrAqoHOx6TFgomS",
-    agencyMonthly: "price_1TGoeSJRdCrAqoHOopnsrHwO",
-    agencyAnnual: "price_1TGoetJRdCrAqoHOXGliTSr9",
+    starterMonthly: "price_1TJyR4JV85dwM5kFnc69LYn9",
+    starterAnnual: "price_1TJyR3JV85dwM5kF3XV7chxW",
+    proMonthly: "price_1TJyR2JV85dwM5kFE1fS6u2b",
+    proAnnual: "price_1TJyR3JV85dwM5kFyKmoamhR",
+    agencyMonthly: "price_1TJyR2JV85dwM5kFAzgIpeSx",
+    agencyAnnual: "price_1TJyR5JV85dwM5kFdBTobeQN",
   },
 } as const;
 
